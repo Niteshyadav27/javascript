be2b8673d3cd85c4b5c2cc85ed45179ff267f923
niteshyadav27 <niteshyaduvanshi2007@gmail.com>
2026-07-05 15:53:06 +0530

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all ts code as newer versipn
+let name = "nitesh"
+let age = 18
+let isloggedIn = false
+
+// number =>2 to power 53
+//bigint
+//string=> ""
+// boolean => true/false
+// null => standlone type
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof "nitesh");
