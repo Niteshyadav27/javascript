f4f9d5625c67c50b4f5a667124f1d2675c25d13e
niteshyadav27 <niteshyaduvanshi2007@gmail.com>
2026-07-23 21:09:58 +0530

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+// ARRAY
+
+const myArr = [0,1,2,3,4,5]
+const myHeros =["Hulk", "ironman", "thor"]
+
+myArr2 = new Array(1,2,3,4,5)
+console.log(myArr[0]);
+console.log(myHeros);
+
+//    Array methods
+
+myArr.push(6)
+myArr.push(7)
+
+console.log(myArr);
+
