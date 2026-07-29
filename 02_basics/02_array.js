160b4c2743f74a93de06cb0b487cdaba1c3aed2c
niteshyadav27 <niteshyaduvanshi2007@gmail.com>
2026-07-30 00:34:30 +0530

    array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,11 @@
+const marvel_heros = ["hulk", "thor", "irpnman"]
+const dc_heros =["superman","batman", "flash"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros[3][0]);
+// console.log(marvel_heros);
+
+
+const news  = marvel_heros.concat(dc_heros)
+console.log(news);
+
