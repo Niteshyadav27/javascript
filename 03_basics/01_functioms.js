ee3e617c85f8028a4c0393167d8d852c90ef349c
niteshyadav27 <niteshyaduvanshi2007@gmail.com>
2026-08-11 00:00:37 +0530

    function

---
+++ 03_basics/01_functioms.js
@@ -0,0 +1,47 @@
+
+function sayMyName(){
+    console.log("N");
+console.log("I");
+console.log("T");
+console.log("E");
+console.log("S");
+console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 +number2);
+    
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1+ number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3,5)
+
+// console.log("result:", result);
+
+
+function loginUserMessage(username){
+    if(username ==undefined){
+        console.log("please enter your username")
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage());
+// console.log(loginUserMessage("Nitesh"));
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+
+const user =
+
+console.log(calculateCartPrice(200,300,400));
